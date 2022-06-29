86d7da2be1a94a2f2122981c5e78d7bfbbde3340
Gabriel Garcia <agguamang@itsjapon.edu.ec>
2022-06-29 00:11:21 -0500

    first commit

---
+++ src/test/java/com/gabrielcodigo/com/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcodigo.com.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
